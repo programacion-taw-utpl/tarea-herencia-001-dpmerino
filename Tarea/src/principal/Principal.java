5ee5f3374002f727ae3d404fca724c9554f4b675
Diego Merino <diegopaul7@icloud.com>
2017-11-11 16:34:32 -0500

    Tarea

--- Tarea/src/principal/Principal.java
+++ Tarea/src/principal/Principal.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tarea;
+package principal;
 import laempresa.EmpresaPrivada;
 import personal.Trabajador;
 /**
